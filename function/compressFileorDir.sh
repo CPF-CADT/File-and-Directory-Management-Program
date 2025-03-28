b918403302dd60088e38954a7e36680205e6bec9
Choun Rathanak <Nak4131@gmail.com>
2025-03-28 10:54:13 +0700

    Complete CompressFile Feature

--- function/compressFileorDir.sh
+++ function/compressFileorDir.sh
@@ -50,3 +50,40 @@ compress() {
     echo "Compression successful: $fileCompress"
     trackAction "compress $content into $fileCompress"
 }
+uncompress() {
+    checkSpcialChar "$1"
+    fileCompress="$1"
+    content="$2"
+    checkFileValidation "$content"
+
+    if [ -f "$content" ]; then
+        case "$content" in
+            *.zip)
+                echo "Extracting zip file: $content"
+                unzip "$content" -d "$fileCompress"  
+                ;;
+            *.tar)
+                echo "Extracting tar file: $content"
+                tar -xvf "$content" -C "$fileCompress" 
+                ;;
+            *.gz)
+                echo "Extracting gz file: $content"
+                gzip -d "$content"  
+                mv "${content}.gz" "$fileCompress"  
+                ;;
+            *)
+                echo "Unsupported file type: $content"
+                return 1
+                ;;
+        esac
+    elif [ -d "$content" ]; then
+        echo "Content is a directory: $content"
+        return 1
+    else
+        echo "Content not found: $content"
+        return 1
+    fi
+
+    echo "Uncompression successful: $fileCompress"
+    trackAction "uncompress $content into $fileCompress"
+}
